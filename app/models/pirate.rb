868a5212f905ffe2661a801ad2a9079126d9833f
JesseNReynolds <jessenreynolds@gmail.com>
2020-11-11 10:33:47 -0700

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,21 @@
 class Pirate
+
+    attr_accessor :name, :weight, :height
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all = []
+    end
+
+    def initialize (hash)
+        @name = hash["name"]
+        @weight = hash["weight"]
+        @height = hash["height"]
+    end
+    
 end
